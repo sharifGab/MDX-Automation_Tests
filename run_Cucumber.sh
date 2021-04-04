c3a80d53b5566bc9a47b366161f934a5bbe4ef6a
Sharif Gab-Allah <sharif1984@yahoo.com>
2021-04-03 22:31:33 -0400

    update files

---
+++ run_Cucumber.sh
@@ -0,0 +1,3 @@
+mvn clean install 
+-Dcucumber.glue="step_definitions" 
+   -Dcucumber.plugin="pretty src/test/resources/project/belly.feature"
--- src/test/java/step_definations/RunCucumberTest.java
+++ src/test/java/step_definations/RunCucumberTest.java
@@ -5,19 +5,10 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        features = {"src/test/resources/project"},
-        //glue = {"step_definations"},
-        tags = "@sgab-allah",
-        //monochrome = true,
-        plugin = {"pretty",
-                "html:target/cucumber-html-report",
-                "json:target/cucumber.json",
-                "junit:target/cucumber.xml",
-                "rerun:target/rerun.txt",
-                "pretty:target/cucumber-pretty.xml",
-                "usage:target/cucumber-usage.json"
-        })
-public class RunCucumberTest{
+@CucumberOptions(features = { "src/test/java/features/Regression/belly.feature" }, glue = {
+        "step_definations" }, tags = "@sgab-allah", //monochrome = false,
+         plugin = { "pretty", "html:target/cucumber-html-report", "json:target/cucumber.json", "junit:target/cucumber.xml",
+                "rerun:target/rerun.txt", "pretty:target/cucumber-pretty.xml", "usage:target/cucumber-usage.json" })
+public class RunCucumberTest {
 
 }
